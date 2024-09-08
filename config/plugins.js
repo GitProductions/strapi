bd670b72aec53fae5e55a97db9ca4fa8b5b9d9fd
Gitago <76603653+gitagogaming@users.noreply.github.com>
2024-09-08 12:03:45 -0400

    enabling graphql

--- config/plugins.js
+++ config/plugins.js
@@ -18,4 +18,15 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  graphql: {
+    enabled: true,
+    config: {
+      playgroundAlways: false,
+      defaultLimit: 10,
+      maxLimit: 20,
+      apolloServer: {
+        tracing: true,
+      },
+    }
+  }
 });
